9afc9881fb321741789ebdc0dad768adc1e8988f
Nikita Morozov <nikitamorozka@gmail.com>
2025-03-24 21:14:21 +0400

    ADD

--- src/main/java/com/nikita/todolist/core/services/UserService.java
+++ src/main/java/com/nikita/todolist/core/services/UserService.java
@@ -1,9 +1,7 @@
 package com.nikita.todolist.core.services;
 
 
-import com.nikita.todolist.core.domain.Task;
 import com.nikita.todolist.core.domain.User;
-import com.nikita.todolist.core.repositories.TaskRepository;
 import com.nikita.todolist.core.repositories.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +21,7 @@ public class UserService {
 
     public User loginUser(String email, String password) {
         User user = userRepository.findByEmail(email);
-        if (user != null && user.getPassword().equals(password)) { // лучше сделать отдельный класс валидатор
+        if (user != null && user.getPassword().equals(password)) {
             return user;
         }
         return null;
